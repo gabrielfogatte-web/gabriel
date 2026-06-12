25f05c6c85a8ebf8b71682917b8e7c6ebe128c85
gabrielfogatte-web <gabriel.fogatte@escola.pr.gov.br>
2026-06-12 09:19:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function calcular(operacao, num1, num2) {
+    switch (operacao) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            if (num2 === 0) {
+                return "Erro: divisão por zero";
+            }
+            return num1 / num2;
+        default:
+            return "Operação inválida";
+    }
+}
 
+// Exemplos de uso
+console.log(calcular("+", 10, 5)); // 15
+console.log(calcular("-", 10, 5)); // 5
+console.log(calcular("*", 10, 5)); // 50
+console.log(calcular("/", 10, 5)); // 2
